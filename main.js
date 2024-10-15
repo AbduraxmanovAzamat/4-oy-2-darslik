d4c69cbd2490bbccc59c71725375cd4fb81a1225
AbduraxmanovAzama <azamatabduraxmonov25@gmail.com>
2024-10-15 23:35:18 +0500

    yukladim

---
+++ main.js
@@ -0,0 +1,155 @@
+// masalalar
+
+// 4-masala
+
+// let a = +prompt("son kiriting")
+// let b = +prompt("son kiriting")
+// let s = +prompt("son kiriting")
+// let musbat_sonlar =0
+
+
+// if(a>0){
+//     musbat_sonlar++
+// }
+// if(b>0) {
+//     musbat_sonlar++
+// }
+
+// if(s>0){
+//     musbat_sonlar++
+// }
+
+// console.log("musbat  " + musbat_sonlar);
+
+// 5-masala
+
+// let a = +prompt("son kiriting")
+// let b = +prompt("son kiriting")
+// let s = +prompt("son kiriting")
+// let musbat_sonlar =0
+// let manfiy_sonlar =0
+
+// if(a>0){
+//     musbat_sonlar++
+// }
+// else if(a<0){
+//     manfiy_sonlar++
+// }
+// if(b>0){
+//     musbat_sonlar++
+// }
+// else if(b<0){
+//     manfiy_sonlar++
+// }
+// if(s>0){
+//     musbat_sonlar++
+// }
+// else if(s<0){
+//     manfiy_sonlar++
+// }
+// console.log("musbat  " + musbat_sonlar + " manfiy " + manfiy_sonlar);
+
+
+// 6-masala
+
+// let a = +prompt("son kiriting")
+// let b = +prompt("son kiriting")
+// let kattasi 
+
+// if (a>b){
+//     kattasi=a
+// }
+    
+// if(b>a){
+//     kattasi=b
+// }
+
+// console.log("kattasi " + kattasi)
+
+// 7-masala
+
+// let a = +prompt("son kiriting")
+// let b = +prompt("son kiriting")
+// let kichik_son
+// let tartib_raqam
+
+// if (a<b){
+//     kichik_son=a
+//     tartib_raqam=1
+// }
+
+// else if(b<a){
+//     kichik_son=b
+//     tartib_raqam=2
+// }
+
+// console.log("kichik son: " + kichik_son + " tartib raqam: " + tartib_raqam);
+
+
+// 8-masala
+
+// let a = +prompt("son kiriting")
+// let b = +prompt("son kiriting")
+
+// if(a>b){
+//     console.log("Katta son: " + a);
+//     console.log("Kichik son: " + b);
+// }
+
+// if (a<b){
+//     console.log("Katta son: " + b);
+//     console.log("Kichik son: " + a)
+// }
+
+// 9-masala
+
+// let a = +prompt("a ning qiymatini kiriting");
+// let b = +prompt("b ning qiymatini kiriting");
+
+// if (a >= b) {
+
+//     let t = a;
+//     a = b;
+//     b = t;
+// }
+
+// console.log("a =", a);
+// console.log("b =", b);
+
+// 10 - masala
+
+// let a = +prompt("a butun sonni kiriting:");
+// let b = +prompt("b butun sonni kiriting:");
+
+
+// if (a !== b) {
+//     let s = a + b;
+//     a = s;
+//     b = s; 
+
+// } else {
+//     a = 0;
+//     b = 0;
+// }
+
+// console.log("a:", a);
+// console.log("b:", b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
